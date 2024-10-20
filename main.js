43b661d8e445913d6de272a44fd5270d4e73777f
Elrit <xelritx@gmail.com>
2024-10-20 19:47:54 +0200

    update

--- main.js
+++ main.js
@@ -30,29 +30,71 @@ class Scramble {
 
 class Settings {
 	constructor() {
+		this.bg1ColorElement = document.getElementById("bg-color-1");
+		this.bg2ColorElement = document.getElementById("bg-color-2");
 		this.clearElement = document.getElementById("clear-button");
 		this.exportElement = document.getElementById("export-button");
+		this.fontColorElement = document.getElementById("font-color");
 		this.fontSelectElement = document.getElementById("font-select");
+		this.defaultSettingsElement = document.getElementById("default-settings");
 		this.load();
 		this.handleInput();
 	}
 
 	handleInput() {
-		this.clearElement.onclick = () => this.clear();
-		this.exportElement.onclick = () => solves.export();
+		this.clearElement.addEventListener("click", () => {
+			if (!confirm("Delete all solves?")) {
+				return;
+			}
+			solves.json = [];
+			stats.json = {};
+			location.reload();
+		});	
+		this.exportElement.addEventListener("click", () => {
+			solves.export();
+		});		
+		this.bg1ColorElement.addEventListener("change", () => {
+			this.json.bgColor1 = this.bg1ColorElement.value;
+		    document.documentElement.style.setProperty("--bg-color-1", this.json.bgColor1);  
+		});
+		this.bg2ColorElement.addEventListener("change", () => {
+			this.json.bgColor2 = this.bg2ColorElement.value;
+		    document.documentElement.style.setProperty("--bg-color-2", this.json.bgColor2);  
+		});		
+		this.fontColorElement.addEventListener("change", () => {
+			this.json.fontColor = this.fontColorElement.value;
+		    document.documentElement.style.setProperty("--font-color", this.fontColorElement.value);  
+		});				
 		this.fontSelectElement.addEventListener("change", function() {
 		    document.documentElement.style.setProperty("--font-family", this.value); 
 		});
-	}
-
-	clear() {
-		localStorage.clear();
-		location.reload();
+		this.defaultSettingsElement.addEventListener("click", () => {
+			this.json = {
+				bgColor1: "#111111", 
+				bgColor2: "#222222",
+				fontColor: "#ffffff", 
+				fontFamily: "Verdana" 
+			};	
+			location.reload();	
+		});		
 	}
 
 	load() {
 		this.json = localStorage.getItem("settings");
-		this.json = this.json ? JSON.parse(this.json) : [];
+		this.json = this.json ? JSON.parse(this.json) : {
+			bgColor1: "#111111", 
+			bgColor2: "#222222",
+			fontColor: "#ffffff", 
+			fontFamily: "Verdana" 
+		}; 
+		document.documentElement.style.setProperty("--bg-color-1", this.json.bgColor1);  
+		document.documentElement.style.setProperty("--bg-color-2", this.json.bgColor2);  
+		document.documentElement.style.setProperty("--font-color", this.json.fontColor); 
+		document.documentElement.style.setProperty("--font-family", this.json.fontFamily); 
+		this.bg1ColorElement.value = this.json.bgColor1;
+		this.bg2ColorElement.value = this.json.bgColor2;
+		this.fontColorElement.value = this.json.fontColor;
+		this.fontSelectElement.value = this.json.fontFamily;
 	}
 }
 
@@ -73,7 +115,8 @@ class Solves {
 	}
 
 	load() {
-		this.json = this.json ? JSON.parse(this.json) : [];
+		this.json = localStorage.getItem("solves");		
+		this.json = this.json ? JSON.parse(this.json) : [];			
 		this.jsonHtml = this.json.map(solve => `<li>${solve.time}</li>`).join('');
 		this.element.innerHTML = this.jsonHtml;
 	}
@@ -88,7 +131,7 @@ class Solves {
    		const blob = new Blob([JSON.stringify(this.json)], { type: "application/json" });
     	const link = document.createElement('a');
 		link.href = URL.createObjectURL(blob);
-		link.download = timer.getDate(new Date());
+		link.download = main.getDate(new Date());
         link.click();		
 	}
 }
@@ -101,18 +144,20 @@ class Stats {
 	}
 
 	load() {
-		this.stats = localStorage.getItem("stats");
-		this.stats = this.stats ? JSON.parse(this.stats) : {};
-		if (!this.stats.pb == undefined) {
-			this.element.innerHTML = this.stats.pb;
-		}
+		this.json = localStorage.getItem("stats");
+		console.log("before: " + this.json);		
+		this.json = this.json ? JSON.parse(this.json) : {};	
+		console.log("after: " + this.json);	
+		this.element.innerHTML = this.json.pb === undefined ? "-" : this.json.pb;
 	}
 
 	update(time) {
-		if (this.stats.pb == undefined || time < this.stats.pb) {
-			this.stats.pb = time;
-			this.element.innerHTML = this.stats.pb;
-		}
+		if (this.json.pb == undefined || time < this.json.pb || !this.json.pb) {
+			this.json.pb = time;
+			this.element.innerHTML = this.json.pb;	
+			localStorage.setItem("stats", JSON.stringify(this.json));	
+		}	
+		console.log(localStorage.getItem("stats"));
 	}
 }
 
@@ -158,20 +203,13 @@ class Timer {
 		this.element.innerHTML = ((Date.now() - this.startTime) / 1000).toFixed(2);
 	}
 
-	getDate(date) {
-		const year = date.getFullYear(); 
-		const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
-		const day = date.getDate().toString().padStart(2, '0'); 
-		return `${day}-${month}-${year}`; 
-	}
-
 	stop() {
 		this.solveTime = ((Date.now() - this.startTime) / 1000);
 		this.solveTime = Math.floor(this.solveTime * 100) / 100;
 		this.pressed = true;
 		this.running = false;
 		this.element.innerHTML = this.solveTime;
-		solves.add(new Solve(this.solveTime, scramble.element.innerHTML, this.getDate(new Date())));		
+		solves.add(new Solve(this.solveTime, scramble.element.innerHTML, main.getDate(new Date())));		
 		scramble.element.innerHTML = scramble.generate();
 		clearInterval(this.interval);		
 	}
@@ -186,14 +224,23 @@ class Main {
 		window.scramble = new Scramble();
 		window.settings = new Settings();
 	}
+
+	getDate(date) {
+		const year = date.getFullYear(); 
+		const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
+		const day = date.getDate().toString().padStart(2, '0'); 
+		return `${day}-${month}-${year}`; 
+	}
+
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-	const main = new Main();
+	window.main = new Main();
 });
 
 window.addEventListener("beforeunload", () => {
-    localStorage.setItem("solves", JSON.stringify(solves.solves));
-    localStorage.setItem("stats", JSON.stringify(stats.stats));
+	localStorage.setItem("settings", JSON.stringify(settings.json));
+    localStorage.setItem("solves", JSON.stringify(solves.json));
+    localStorage.setItem("stats", JSON.stringify(stats.json));
 });
